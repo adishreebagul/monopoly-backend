5e6c0a2a1781a879b5fe53b84853621ee7fefef8
adishreebagul <adishreebagul97@gmail.com>
2025-12-25 00:03:23 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+import mongoose from './db.js'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST']
+}))
+
+app.get('/', (req, res) => {
+    res.send('Monopoly backend running')
+})
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('A player connected', socket.id)
+
+    socket.on('playerMove', (data) => {
+        io.emit('updateGame', data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('A player disconnected', socket.id)
+    })
+})
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
