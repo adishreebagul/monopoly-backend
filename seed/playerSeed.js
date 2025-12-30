a0078aaaa257153df4a55c92049d1eea71a1e80a
adishreebagul <adishreebagul97@gmail.com>
2025-12-30 12:55:49 +0530

    new add

---
+++ seed/playerSeed.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import Player from "../models/Player.js";
+
+const seedPlayers = async () => {
+    await Player.deleteMany()
+    const players = await Player.insertMany([
+        { name: 'Alice', money: 1500, position: 0 },
+        { name: 'Bob', money: 1500, position: 0 },
+        { name: 'Charlie', money: 1500, position: 0 }
+    ])
+    console.log('Players created', players.map(p => p._id))
+    process.exit()
+}
+
+seedPlayers()
