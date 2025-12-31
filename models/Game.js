e62ca4d88449dcf2779776bff45fbd48eaa609bb
adishreebagul <adishreebagul97@gmail.com>
2025-12-31 20:06:33 +0530

    new change

--- models/Game.js
+++ models/Game.js
@@ -6,9 +6,7 @@ const gameSchema = new mongoose.Schema({
     players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
     board: [{
         property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' },
-        owner: {
-            type: mongoose.Schema.Types.ObjectId, ref: 'Player', default: null
-        }
+        owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', default: null }
     }],
     turnIndex: { type: Number, default: 0 },
     status: {
@@ -16,7 +14,6 @@ const gameSchema = new mongoose.Schema({
         enum: ['waiting', 'started', 'ended'],
         default: 'waiting'
     }
-
 }, { timestamps: true })
 
 gameSchema.methods.getCurrentPlayerId = function () {
